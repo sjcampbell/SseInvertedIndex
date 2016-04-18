3b8e1a62051aed33e85d407aaaab1ced6f179b64
Sam Campbell <sjcampbell40@gmail.com>
2016-04-18 01:19:46 -0400

    Updated query script

--- query.sh
+++ query.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-echo "Executing query for: $1"
+echo "Executing query for: $3"
 
-java -cp target/classes:target/bigdata-0.1.0-SNAPSHOT.jar com.sjcampbell.spark.sse.retrieval.BooleanRetrieval --index indexOutput --data-path data/Shakespeare.txt --query $1
+hadoop jar target/bigdata-0.1.0-SNAPSHOT.jar com.sjcampbell.spark.sse.retrieval.BooleanRetrievalJava\
+	      -index $1 \
+		  -collection $2 \
+		  -query $3
